811b25579326082001086b72a97bd4f0219d9e96
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-01-12 19:48:20 +0100

    Add Travis CI autodeploy to GitHub Pages

---
+++ gh-pages-deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+ember build --environment production
+
+cd dist
+
+git init
+
+git config user.name "Kevin P. Kucharczyk"
+git config user.email "kevinkucharczyk@gmail.com"
+
+git add .
+git commit -m "Deploy to GitHub Pages"
+
+echo "Pushing to GitHub..."
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
