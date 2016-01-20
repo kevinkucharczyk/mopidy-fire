319b560d3852466fc5aeced8de897eb8f15d71c7
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-01-20 17:59:08 +0100

    Modify loader service to manage multiple calls

--- app/services/loader-service.js
+++ app/services/loader-service.js
@@ -1,15 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Service.extend({
+  callCount: 0,
   show() {
-    Ember.run.next(() => {
-      Ember.$('.loader').show();
-    });
+    this.incrementProperty('callCount');
+    if (this.get('callCount') === 1) {
+      Ember.run.next(() => {
+        Ember.$('.loader').show();
+      });
+    }
   },
 
   hide() {
-    Ember.run.next(() => {
-      Ember.$('.loader').hide();
-    });
+    this.decrementProperty('callCount');
+    if (this.get('callCount') < 0) {
+      this.set('callCount', 0);
+    }
+
+    if (this.get('callCount') === 0) {
+      Ember.run.next(() => {
+        Ember.$('.loader').hide();
+      });
+    }
   }
 });
