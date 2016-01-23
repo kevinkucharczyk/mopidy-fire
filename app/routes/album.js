ea6312f21bb24b1afda02c73534d061fba94bb1b
Kevin P. Kucharczyk <kevinkucharczyk@gmail.com>
2016-01-23 11:39:53 +0100

    Modify album views
    Add play/add buttons to album route

--- app/routes/album.js
+++ app/routes/album.js
@@ -3,5 +3,25 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.get('mopidy').getAlbum(params.uri);
+  },
+
+  _playAlbum() {
+    let uri = this.get('controller.model.album.uri');
+    this.get('mopidy').clearAndPlaySingle(uri);
+  },
+
+  _addAlbum() {
+    let uri = this.get('controller.model.album.uri');
+    this.get('mopidy').addTrack(uri);
+  },
+
+  actions: {
+    play() {
+      this._playAlbum();
+    },
+
+    add() {
+      this._addAlbum();
+    }
   }
 });
